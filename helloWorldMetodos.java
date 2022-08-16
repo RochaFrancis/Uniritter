c98e496555ef5d429efea1643a338c94a7dfbd10
Francisco Rocha <108292369+RochaFrancis@users.noreply.github.com>
2022-08-16 12:55:10 -0300

    Add files via upload

---
+++ helloWorldMetodos.java
@@ -0,0 +1,16 @@
+public class helloWorldMetodos {
+    static void olaMetodos (){
+
+        System.out.println ("Sou um novo método!");
+    }
+    public static void main(String[] args) {
+
+        System.out.println ("Começando a execução do programa.");
+
+        olaMetodos();
+
+        System.out.println ("Terminando a execução do programa.");
+
+    }
+}
+
